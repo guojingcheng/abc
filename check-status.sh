45683315a9da8cdd2db5af7ff4d2974d07a4034e
GoCD User <go@ip-10-253-206-43.cn-northwest-1.compute.internal>
2019-06-05 12:34:12 +0000

    Initial check-status.sh

---
+++ check-status.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -x
+
+getAddress(){
+    ip=$(dig abc.openlocation.cn +short)
+    echo $ip
+}
+
+abc=$(getAddress)
+echo $abc
+
+for((i=1;i<=10;i++));  
+do   
+    echo
+    sleep 60
+    tmp=$(getAddress)
+    echo $tmp
+    if [ $tmp != $abc ]; then 
+        echo "abc"
+        exit 0
+    else
+        echo "not ready"
+    fi
+done 
+
+exit 1
